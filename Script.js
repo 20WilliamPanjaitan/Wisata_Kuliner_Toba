d5b4317e062c5deda157adb07db42fa77685c16e
20WilliamPanjaitan <20williampanjaitan@gmail.com>
2023-09-07 21:02:55 +0700

    first commit

---
+++ Script.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function () {
+  //If it is visible, fade-out; Otherwise, fade-in
+  $(':button[id="btnFadeToogle"]').click(function () {
+    $('#logo1').fadeToggle(700);
+
+  })
+});
+
+$(document).ready(function () {
+  //If it is visible, fade-out; Otherwise, fade-in
+  $(':button[id="btnFadeToogle"]').click(function () {
+    $('#logo2').fadeToggle(1400);
+
+  })
+});
+
+$(document).ready(function () {
+  //If it is visible, fade-out; Otherwise, fade-in
+  $(':button[id="btnFadeToogle"]').click(function () {
+    $('#logo3').fadeToggle(2100);
+
+  })
+});
+
+$(document).ready(function () {
+  //If it is visible, fade-out; Otherwise, fade-in
+  $(':button[id="btnFadeToogle"]').click(function () {
+    $('#logo4').fadeToggle(2800);
+
+  })
+});
